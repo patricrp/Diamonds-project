593720d35019b2f68be68bf0debbbe28ed4fbc5c
patricp <publizistik.try@gmail.com>
2020-03-01 00:09:45 +0100

    Several models tested

--- models_fit_rmse.py
+++ models_fit_rmse.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, BaggingRegressor
 from sklearn.metrics import *
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def algoModel(algorithms, X, y):
+def algoModel(di, X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    for k,v in algorithms:
+    for k,v in di.items():
         score = v.score(X_test,y_test)
-        rmse = np.sqrt(mean_squared_error(y_test, k.predict(X_test)))
-        y_pred = k.predict(X_test)
+        rmse = np.sqrt(mean_squared_error(y_test, v.predict(X_test)))
+        y_pred = v.predict(X_test)
         
         print(k)
         print('Score is ', score)
@@ -20,3 +20,4 @@ def algoModel(algorithms, X, y):
         sns.scatterplot(y_pred, y_test)
         plt.xlabel("Predicted price")
         plt.ylabel("Real price")
+        plt.show()
